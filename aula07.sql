197d2d80f76b79cfaa11da43e7989d9850a7a426
danzinho007 <sedutordanzinho@hotmail.com>
2023-03-03 03:34:49 -0300

    alterei

--- aula07.sql
+++ aula07.sql
@@ -3,4 +3,3 @@
 RENAME TABLE nome_antigo TO nome_novo;
 Exemplo :
 RENAME TABLE pessoas1 TO pessoas2;
-
--- aula10.sql
+++ aula10.sql
@@ -4,4 +4,3 @@ ALTER TABLE nome_tabela DROP PRIMARY KEY;
 
 Exemplo :
 ALTER TABLE pessoas1 DROP PRIMARY KEY;
-
